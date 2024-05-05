eda55e2ca6040e93b72ec0aaf05172ad558d81de
dvirlabs <dvirlabs@gmail.com>
2024-05-05 03:08:59 +0300

    change font

--- frontend/src/components/easyTargetLogo.tsx
+++ frontend/src/components/easyTargetLogo.tsx
@@ -3,9 +3,9 @@ import logo from '../icons/easy-target-logo.png'
 
 const EasyTargetLogo = () => {
     return (
-        <div className='easy-target-logo'>
-            <h1>Easy Target</h1>
-            <img className='easy-target-logo' src={logo} alt='logo'></img>
+        <div className='easy-target'>
+            <h1 className='easy-target'>Easy Target</h1>
+            <img className='easy-target' src={logo} alt='logo'></img>
         </div>
     )
 }
