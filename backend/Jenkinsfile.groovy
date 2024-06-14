705b68415e8b3a5fd32181c8864f6172697933ef
dvirlabs <dvirlabs@gmail.com>
2024-06-14 18:30:55 +0300

    api check

--- backend/Jenkinsfile.groovy
+++ backend/Jenkinsfile.groovy
@@ -22,6 +22,25 @@ pipeline {
             }
         }
 
+        stage("test") {
+            steps {
+                echo 'Run and test the Docker container...'
+                script {
+                    sh "docker run -d --name backend-container -p 8000:8000 ${DOCKER_IMAGE}"
+                    sleep 10 // Wait for the container to be fully up and running
+                    try {
+                        sh 'curl -f http://localhost:8000/get_targets'
+                        echo 'API test passed'
+                    } catch (Exception e) {
+                        error("API test failed: ${e.message}")
+                    } finally {
+                        sh 'docker stop backend-container'
+                        sh 'docker rm backend-container'
+                    }
+                }
+            }
+        }
+
         stage("Push image") {
             steps {
                 echo 'push to dockerhub...'
