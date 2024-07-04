5747bab0bb12824f7c1e319121ddc46cb3c44f29
dvirlabs <dvirlabs@gmail.com>
2024-07-04 19:05:03 +0300

    add docker network

--- backend/Jenkinsfile.groovy
+++ backend/Jenkinsfile.groovy
@@ -5,7 +5,6 @@ pipeline {
     }
 
     stages {
-
         stage("build") {
             steps {
                 sh "docker build -t ${DOCKER_IMAGE} backend/."
@@ -25,35 +24,42 @@ pipeline {
             steps {
                 echo 'Run and test the Docker container...'
                 script {
-                    sh "docker run -d --name backend-container -p 8000:8000 ${DOCKER_IMAGE}" 
+                    sh "docker network create test-network"
+
+                    sh "docker run -d --name backend-container --network test-network -p 8000:8000 ${DOCKER_IMAGE}"
                     sleep 10 // Wait for the container to be fully up and running
-                    sh  '''
-                        docker run -d --name prometheus --rm -p 9090:9090 -v $(pwd)/targets.yml:/etc/prometheus/targets.yml -v $(pwd)/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
+                    
+                    sh '''
+                        docker run -d --name prometheus --network test-network --rm -p 9090:9090 \
+                        -v $(pwd)/targets.yml:/etc/prometheus/targets.yml \
+                        -v $(pwd)/prometheus.yml:/etc/prometheus/prometheus.yml \
+                        prom/prometheus
                     '''
                     sleep 10
+                    
                     try {
                         sh '''
-                            curl -X GET http://localhost:8000/get_targets
+                            curl -X GET http://backend-container:8000/get_targets
                         '''
                         // Test add_target API
                         sh '''
-                            curl -X POST http://localhost:8000/add_target \
+                            curl -X POST http://backend-container:8000/add_target \
                                  -H "Content-Type: application/json" \
-                                 -d \'{"target_ip": "8.8.8.8", "port": 1111}\'
+                                 -d '{"target_ip": "8.8.8.8", "port": 1111}'
                         '''
                         echo 'add_target API test passed'
 
                         // Test get_targets API
                         sh '''
-                            curl -XGET http://localhost:8000/get_targets
+                            curl -X GET http://backend-container:8000/get_targets
                         '''
                         echo 'get_targets API test passed' 
                         
                         // Test remove_target API
                         sh '''
-                            curl -X DELETE http://localhost:8000/remove_target \
+                            curl -X DELETE http://backend-container:8000/remove_target \
                                  -H "Content-Type: application/json" \
-                                 -d \'{"target_ip": "8.8.8.8", "port": 1111}\'
+                                 -d '{"target_ip": "8.8.8.8", "port": 1111}'
                         '''
                         echo 'remove_target API test passed'
                     } catch (Exception e) {
@@ -63,6 +69,7 @@ pipeline {
                         sh 'docker rm backend-container'
                         sh 'docker stop prometheus'
                         sh 'docker rm prometheus'
+                        sh 'docker network rm test-network'
                     }
                 }
             }
@@ -74,13 +81,14 @@ pipeline {
                 sh "docker push ${DOCKER_IMAGE}"
             }
         }
-
     }
 
     post {
         always {
-            // Clean up docker containers in case of failure
+            // Clean up docker containers and network in case of failure
             sh 'docker rm -f backend-container || true'
+            sh 'docker rm -f prometheus || true'
+            sh 'docker network rm test-network || true'
         }
     }
 }
